5994525cdebe1ebd1ed8cfa462de6b8c2ea14b63
Mckolesidou <georgia.kolesidou@gmail.com>
2021-07-05 22:09:38 +0300

    Fundamentals 5

--- Fundamentals 4/reverseString/reverseString.js
+++ Fundamentals 4/reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function (str) {
-    var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) {
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
         newString += str[i];
     }
     return newString;
